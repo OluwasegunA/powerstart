e994f0d7fafffff8e9e8ef9a0c3566a1dffd5c2f
SegunAriyo <segun.ariyo@globalaccelerex.com>
2024-02-02 10:29:23 +0000

    Initial push after completing task 1 and 2

--- nifty/__main__.py
+++ nifty/__main__.py
@@ -3,6 +3,8 @@ from starlette.responses import JSONResponse
 from starlette.requests import Request
 from starlette.routing import Route
 import uvicorn
+import csv
+from datetime import datetime
 
 
 async def price_data(request: Request) -> JSONResponse:
@@ -10,14 +12,66 @@ async def price_data(request: Request) -> JSONResponse:
     Return price data for the requested symbol
     """
     symbol = request.path_params['symbol']
+    year = request.query_params.get('year', None)
+    
+    if not validateSymbol(symbol) :
+        return JSONResponse({'error': 'Invalid character provided in symbol'}, status_code=400)
+    
+    year = validateYear(year)
+    
+    res = getData(symbol=symbol, year=year)
+    
+    res = sorted(res, key = getDate)
+    
+    return JSONResponse(res)
 
-    # TODO:
-    # 1) Return open, high, low & close prices for the requested symbol as json records
-    # 2) Allow calling app to filter the data by year using an optional query parameter
 
-    # Symbol data is stored in the file data/nifty50_all.csv
+# Todo: Service level logic. To be Refactored into a separate file    
+def getDate(obj):
+    return datetime.strptime(obj['date'], '%d/%m/%Y')
 
-    return JSONResponse({'implement': 'me'})
+def getData(symbol, year) -> []:
+    res = []
+    
+    with open('data/nifty50_all.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            
+            if(row['Symbol'].lower() == symbol):
+                row = {k.lower(): v for k, v in row.items()}
+                row.pop('symbol', None)
+                dataDate = datetime.strptime(row['date'], '%Y-%m-%d')
+                formattedDate = dataDate.strftime('%d/%m/%Y')
+                
+                if (year is not None):
+                    if year == dataDate.year  :
+                        row['date'] = formattedDate
+                        res.append(row)
+                        
+                else :
+                    row['date'] = formattedDate
+                    res.append(row)
+    
+    return res
+
+def validateSymbol(sym) -> bool:
+    allowed_characters = set("&abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_ ")
+
+    for char in sym:
+        if char not in allowed_characters:
+            return False
+        
+    return True
+
+def validateYear(yr) -> int:
+    if yr is not None:
+        try:
+            yr = int(yr)
+            if not (1000 <= yr <= 9999):
+                raise ValueError('Year is not a four digit integer')
+            return yr
+        except ValueError as e:
+            return JSONResponse({'error': str(e)}, status_code=400)
 
 # URL routes
 app = Starlette(debug=True, routes=[
@@ -30,7 +84,6 @@ def main() -> None:
     start the server
     """
     uvicorn.run(app, host='0.0.0.0', port=8888)
-
-
+    
 # Entry point
 main()
